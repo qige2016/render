5fbaf3c7b03b025f40024bdc49d5045d68575abf
qige2016 <286882998@qq.com>
2020-11-16 15:06:00 +0800

    feat: add patchPortal

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import { h, Fragment } from './h'
+import { h, Portal } from './h'
 import render from './render'
 
 // 旧的 VNode
-const prevVNode = h(Fragment, null, [
-  h('p', null, '旧片段子节点 1'),
-  h('p', null, '旧片段子节点 2')
-])
+const prevVNode = h(
+  Portal,
+  { target: '#old-container' },
+  h('p', null, '旧的 Portal')
+)
 
 // 新的 VNode
-const nextVNode = h(Fragment, null, [
-  h('p', null, '新片段子节点 1'),
-  h('p', null, '新片段子节点 2')
-])
+const nextVNode = h(
+  Portal,
+  { target: '#new-container' },
+  h('p', null, '新的 Portal')
+)
 
 render(prevVNode, document.getElementById('app'))
 
--- src/render.js
+++ src/render.js
@@ -101,6 +101,30 @@ function mountFragment(vnode, container, isSVG) {
   }
 }
 
+function mountPortal(vnode, container) {
+  const { tag, children, childFlags } = vnode
+
+  // 获取挂载点
+  const target = typeof tag === 'string' ? document.querySelector(tag) : tag
+
+  if (childFlags & ChildrenFlags.SINGLE_VNODE) {
+    // 将 children 挂在到 target 上，而非 container
+    mount(children, target)
+  } else if (childFlags & ChildrenFlags.MULTIPLE_VNODES) {
+    for (let i = 0; i < children.length; i++) {
+      // 将 children 挂在到 target 上，而非 container
+      mount(children[i], target)
+    }
+  }
+
+  // 占位的空文本节点
+  const placeholder = createTextVNode('')
+  // 将该节点挂载到 container 中
+  mountText(placeholder, container, null)
+  // el 属性引用该节点
+  vnode.el = placeholder.el
+}
+
 function mountComponent(vnode, container, isSVG) {
   if (vnode.flags & VNodeFlags.COMPONENT_STATEFUL) {
     mountStatefulComponent(vnode, container, isSVG)
@@ -282,5 +306,42 @@ function patchFragment(prevVNode, nextVNode, container) {
       break
     default:
       nextVNode.el = nextVNode.children[0].el
+  }
+}
+
+function patchPortal(prevVNode, nextVNode) {
+  patchChildren(
+    prevVNode.childFlags,
+    nextVNode.childFlags,
+    prevVNode.children,
+    nextVNode.children,
+    prevVNode.tag // 注意 container 是旧的 container
+  )
+  // 让 nextVNode.el 指向 prevVNode.el
+  nextVNode.el = prevVNode.el
+
+  // 如果新旧容器不同，才需要搬运
+  if (nextVNode.tag !== prevVNode.tag) {
+    // 获取新的容器元素，即挂载目标
+    const container =
+      typeof nextVNode.tag === 'string'
+        ? document.querySelector(nextVNode.tag)
+        : nextVNode.tag
+
+    switch (nextVNode.childFlags) {
+      case ChildrenFlags.SINGLE_VNODE:
+        // 如果新的 Portal 是单个子节点，就把该节点搬运到新容器中
+        container.appendChild(nextVNode.children.el)
+        break
+      case ChildrenFlags.NO_CHILDREN:
+        // 新的 Portal 没有子节点，不需要搬运
+        break
+      default:
+        // 如果新的 Portal 是多个子节点，遍历逐个将它们搬运到新容器中
+        for (let i = 0; i < nextVNode.children.length; i++) {
+          container.appendChild(nextVNode.children[i].el)
+        }
+        break
+    }
   }
 }
