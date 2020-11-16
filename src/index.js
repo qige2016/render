ae414a6481b78b8f63509bba50c259e8434e06af
qige2016 <286882998@qq.com>
2020-11-16 14:51:58 +0800

    feat: add patchFragment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import { h } from './h'
+import { h, Fragment } from './h'
 import render from './render'
 
 // 旧的 VNode
-const prevVNode = h('p', null, '旧文本')
+const prevVNode = h(Fragment, null, [
+  h('p', null, '旧片段子节点 1'),
+  h('p', null, '旧片段子节点 2')
+])
 
 // 新的 VNode
-const nextVNode = h('p', null, '新文本')
+const nextVNode = h(Fragment, null, [
+  h('p', null, '新片段子节点 1'),
+  h('p', null, '新片段子节点 2')
+])
 
 render(prevVNode, document.getElementById('app'))
 
--- src/render.js
+++ src/render.js
@@ -262,3 +262,25 @@ function patchText(prevVNode, nextVNode) {
     el.nodeValue = nextVNode.children
   }
 }
+
+function patchFragment(prevVNode, nextVNode, container) {
+  // 直接调用 patchChildren 函数更新 新旧片段的子节点即可
+  patchChildren(
+    prevVNode.childFlags, // 旧片段的子节点类型
+    nextVNode.childFlags, // 新片段的子节点类型
+    prevVNode.children, // 旧片段的子节点
+    nextVNode.children, // 新片段的子节点
+    container
+  )
+
+  switch (nextVNode.childFlags) {
+    case ChildrenFlags.SINGLE_VNODE:
+      nextVNode.el = nextVNode.children.el
+      break
+    case ChildrenFlags.NO_CHILDREN:
+      nextVNode.el = prevVNode.el
+      break
+    default:
+      nextVNode.el = nextVNode.children[0].el
+  }
+}
